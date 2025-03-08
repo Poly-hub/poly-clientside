36e4c55a376dc747d07705e6be2f231e1959ac3c
ramrevivo <ramrevivo0@gmail.com>
2025-03-08 19:54:57 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+        <h1 className={"title"}>Poly</h1>
+        <h2>Code, Create, Compose</h2>
+    </div>
+  )
+}
+
+export default App
